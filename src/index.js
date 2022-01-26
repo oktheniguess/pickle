20422a9d9ba97747e570d192abc09b15c938c929
oktheniguess <63266962+oktheniguess@users.noreply.github.com>
2022-01-26 12:56:40 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Pickle!</h1>
+<div>
+  Please do no click this
+  <a href="https://www.google.com/search?q=bacon&rlz=1CAJIKU_enUS990&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjWioSLi9D1AhUCj4kEHS2oBBwQ_AUoAXoECAEQAw&biw=1366&bih=617&safe=active&ssui=on#imgrc=oWSEd_h8Jmf6CM" target="_blank" rel="noopener noreferrer">button</a>.
+</div>
+`;
